b18d7e15366a3167fbd28e5674835cd732aeffc7
RUTHVIK SAI MANTHOJU <ruthvikmanthoju@gmail.com>
2026-04-15 01:05:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/royal-reveal-invites/",
+  base: "/Invitation/",
   server: {
     host: "::",
     port: 8080,
